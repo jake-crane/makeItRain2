0e940c4da220a5989ebfb1c4aa62284de131cc1f
Jake Crane <jake@jakecrane.com>
2016-07-11 22:20:13 -0500

    Initial Commit

---
+++ js/makeItRain.js
@@ -0,0 +1,12 @@
+(function ($) {
+    $(function () {
+
+
+       for (var i = 0; i < 15; i++)
+        {
+            var $money = $('<span class="money"/>');
+          $money.appendTo('body');
+        }
+
+    });
+})(jQuery);
